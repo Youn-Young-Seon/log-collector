7f16f4d567a528f44a8df9d80202768ec629164b
szdbsyyszs <szdbsyyszs32@gmail.com>
2025-05-18 00:14:55 +0900

    dtesed

--- src/schema/fw-log.schema.ts
+++ src/schema/fw-log.schema.ts
@@ -10,12 +10,35 @@ export class FwLog extends Document {
   @Prop({
     type: FwConnInfo
   })
-  fwConnInfo: FwConnInfo;
+  fwConnInfo: {
+    natRuleId: number;
+    natRuleName: string;
+    srcIp: string;
+    srcPort: number;
+    dstIp: string;
+    dstPort: number;
+    protocol: number;
+  };
 
   @Prop({
     type: FwBaseInfo
   })
-  fwBaseInfoSchema: FwBaseInfo;
+  fwBaseInfoSchema: {
+    tag: string;
+    startTime: Date;
+    endTime: Date;
+    machineName: string;
+    fwRuleId: number;
+    fwRuleName: string;
+    appProtocol: string;
+    bytesForward: number;
+    bytesBackward: number;
+    bytesTotal: number;
+    terminateReason: string;
+    host: string;
+    srcCountry: string;
+    dstCountry: string;
+  };
 }
 
 export const LogSchema = SchemaFactory.createForClass(FwLog);
